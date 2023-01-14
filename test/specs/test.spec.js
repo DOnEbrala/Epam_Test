73e939fc4d7efb1f8292740693c87450dc9d0920
Nika <ebraebra13@gmail.com>
2023-01-14 18:21:07 +0400

    add specific condition for agree term

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -20,12 +20,16 @@ describe("Test suite", () => {
     
     it("Second Task", async () => {
         await browser.url("https://pastebin.com");
+        const agreeTerm = await $(`//button[@class="sc-ifAKCX ljEJIv"]`);
+        await agreeTerm.waitForDisplayed({timeout:5000 , interval: 3 });
+        await agreeTerm.click();
         const write = await $('//textarea[@id ="postform-text"]');
         const inputOne = await write.addValue(`git config --global user.name "New Sheriff in Town"`)
         await browser.keys(["\uE007"]);
         const inputTwo = await write.addValue(`git reset $ (git commit-tree HEAD ^ {tree} -m "Legacy code")`);
         await browser.keys(["\uE007"]);
         const inputThree = await write.addValue(`git push origin master --force`);
+
         const setDropDown = await $(`//span[@id = "select2-postform-expiration-container"]`).click();
         const choose = await $(`//li[contains(text(), "10 Minutes")]`).click();
         const setTitle = await $(`//input[@id = "postform-name"]`).setValue("how to gain dominance among developers");
@@ -34,7 +38,8 @@ describe("Test suite", () => {
         //checkSecond
 
         await expect(browser).toHaveTitleContaining("how to gain dominance among developers");
-        const codeCheck = await $('//ol[@class = "text"]')
+        // await browser.setTimeout(5000)
+        const codeCheck = await $('//ol[@class = "text"]');
         await expect(codeCheck).toHaveTextContaining('git config --global user.name "New Sheriff in Town"','git reset $ (git commit-tree HEAD ^ {tree} -m "Legacy code")','git push origin master --force');
 
     })
